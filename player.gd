538f879a59bca6e44559d58958828ee06d519ed8
chloecat0 <43815120+TheChaser212@users.noreply.github.com>
2026-01-31 12:47:46 +1300

    movement and tape lines

--- player.gd
+++ player.gd
@@ -1,25 +1,44 @@
 extends CharacterBody2D
 
-var speed: float = 50
+var speed: float = 2000
+var gravity: float = 980
+var jump_impulse: float = 200
 
 var radius: float = 15
 var centre_radius: float = 10
 var empty_radius: float = 7
 
+var radius_reduction: float = 1
+var out_of_tape: bool = false
+
+@onready var collision_circle := $CollisionShape2D
+var collision_shape: CircleShape2D
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	#line_points.append(global_position)
-	pass
-
-func _input(event: InputEvent) -> void:
-	pass
+	collision_shape = CircleShape2D.new()
+	collision_shape.radius = radius
+	collision_circle.shape = collision_shape
 
 func _physics_process(delta: float) -> void:
-	velocity = Vector2.RIGHT*speed
+	if not out_of_tape:
+		velocity.x = speed*delta
+	else:
+		velocity.x = 0
+	if not is_on_floor():
+		velocity.y += gravity*delta
+	elif Input.is_action_just_pressed("jump") && not out_of_tape:
+		velocity.y = -jump_impulse
 	move_and_slide()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	#line_points.append(global_position)
-	#queue_redraw()
-	pass
+	update_radius(radius_reduction*delta)
+
+func update_radius(reduction: float = 0):
+	radius -= reduction
+	if radius < centre_radius:
+		radius = centre_radius
+		out_of_tape = true
+	collision_shape.radius = radius
--- player_draw.gd
+++ player_draw.gd
@@ -1,8 +1,8 @@
 extends Node2D
 
-var radius: float = 15
-var centre_radius: float = 10
-var empty_radius: float = 7
+#var radius: float = 15
+#var centre_radius: float = 10
+#var empty_radius: float = 7
 
 @export var tape_color: Color = Color.BLANCHED_ALMOND
 @export var centre_color: Color = Color.BISQUE
@@ -10,19 +10,32 @@ var empty_radius: float = 7
 
 @onready var body := $Movement
 
-var line_points: PackedVector2Array = PackedVector2Array()
+var line_start: Vector2
+
+var tape_lines: PackedVector2Array = PackedVector2Array()
+var prev_body_on_floor: bool = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	line_points.append(body.global_position)
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	line_points.append(body.global_position)
+func _process(_delta: float) -> void:
+	var body_on_floor: bool = body.is_on_floor()
+	if body_on_floor:
+		if tape_lines.is_empty():
+			tape_lines.append(body.global_position + Vector2(0, body.radius))
+		if body_on_floor != prev_body_on_floor:
+			tape_lines.append(body.global_position + Vector2(0, body.radius))
+		if tape_lines.size() % 2 != 0:
+			tape_lines.append(body.global_position + Vector2(0, body.radius))
+		tape_lines.set(tape_lines.size()-1, body.global_position + Vector2(0, body.radius))
+	prev_body_on_floor = body_on_floor
 	queue_redraw()
 
 func _draw() -> void:
-	draw_circle(body.global_position-Vector2(0,radius), radius, tape_color, true, -1, true)
-	draw_circle(body.global_position-Vector2(0,radius), centre_radius, centre_color, true, -1, true)
-	draw_circle(body.global_position-Vector2(0,radius), empty_radius, background_color, true, -1, true)
-	draw_polyline(line_points, tape_color, 1, true)
+	draw_circle(body.global_position, body.radius, tape_color, true, -1, true)
+	draw_circle(body.global_position, body.centre_radius, centre_color, true, -1, true)
+	draw_circle(body.global_position, body.empty_radius, background_color, true, -1, true)
+	if not tape_lines.is_empty():
+		draw_multiline(tape_lines, tape_color, 1, true)
